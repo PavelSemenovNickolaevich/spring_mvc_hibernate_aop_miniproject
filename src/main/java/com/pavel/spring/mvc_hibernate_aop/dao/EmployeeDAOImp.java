3c35c708f82412523ef2810ebe3c5d7da123c216
pavel <natitshow@gmail.com>
2021-08-10 08:38:30 +0300

    Initial commit

---
+++ src/main/java/com/pavel/spring/mvc_hibernate_aop/dao/EmployeeDAOImp.java
@@ -0,0 +1,26 @@
+package com.pavel.spring.mvc_hibernate_aop.dao;
+
+import com.pavel.spring.mvc_hibernate_aop.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImp implements EmployeeDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        Session session = sessionFactory.getCurrentSession();
+        List<Employee> allEmployees = session.createQuery("from Employee", Employee.class).getResultList();
+
+        return allEmployees;
+    }
+}
